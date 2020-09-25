ec370911f5d7326ddb5f4be09c3feab3dd0a0e04
Diego-mm <diegomorata@hotmail.com>
2020-09-25 17:02:12 +0200

    añadido funcion de click

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
-console.log('hola');
+const boardClick = (event) => {
+  console.log(event.target);
+};
+
+const boardTic = document.querySelector('.boardtic');
+
+boardTic.addEventListener('click', boardClick);
+
+// ELEMENTO //
+const reset = document.querySelector('.reset');
+
+// EVENTO //
+reset.addEventListener('click', resetClick);
+
+// EJECUCION //
+function resetClick(event) {
+  console.log('hola');
+}
